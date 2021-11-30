ffcc82a956d6793f01201705301e39c32ac05397
Mehmet Yuksel <mh.mehmetyuksel@gmail.com>
2021-11-30 17:37:33 +0300

    searchbar added

--- src/components/Categories.js
+++ src/components/Categories.js
@@ -1,10 +1,10 @@
 import "../App.css"
 import React from 'react'
 import { useProduct } from '../contexts/ProductContext'
-import { Form, Container, Button, Row, Col } from 'react-bootstrap'
+import { Form, Container, Button, Row, Col, InputGroup, FormControl, CloseButton } from 'react-bootstrap'
 function Categories() {
 
-  const { setCategory, ProductCategory, setSortPrice, SortPrice } = useProduct();
+  const { setCategory, ProductCategory, setSortPrice, SortPrice, searchValue , setSearchValue } = useProduct();
 
   const handleChange = (text) => (e) => {
     setCategory((prev) => ({
@@ -13,34 +13,51 @@ function Categories() {
     }));
   }
 
+  const handleSearch = (e) => {
+    setSearchValue(e.target.value)
+    console.log(e.target.value)
+  }
+
 
 
   return (
     <Container>
-      <Row>
-        <Col lg={6} style={{ paddingTop: "10px" }}>
-          <Form className="mb-3 text-start d-flex">
+      <Row className="d-flex align-items-center py-2" >
+
+        <Col xl={4} xs={12} >
+          <Form className="text-start d-flex  justify-content-evenly">
             <Form.Check  style={{ marginRight: "5px" }} aria-label="Checkbox for following text input" defaultChecked={ProductCategory.Motherboard ? true : false} label="Motherboards" onChange={handleChange("Motherboard")} />
             <Form.Check  style={{ marginRight: "5px" }} aria-label="Checkbox for following text input" defaultChecked={ProductCategory.Video_Card ? true : false} label="Video Cards" onChange={handleChange("Video_Card")} />
             <Form.Check  style={{ marginRight: "5px" }} aria-label="Checkbox for following text input" defaultChecked={ProductCategory.Processor ? true : false} label="Processors" onChange={handleChange("Processor")} />
           </Form>
         </Col>
 
-        <Col lg={6} >
-          <Form className="d-flex justify-content-lg-end">
-            <div className="form-check  text-start" style={{ paddingTop: "10px" }} >
+
+      <Col xl={4} xs={12} className="mt-1">
+      <InputGroup size="sm" >
+    <InputGroup.Text id="inputGroup-sizing-sm">Search</InputGroup.Text>
+    <FormControl aria-label="Small" aria-describedby="inputGroup-sizing-sm" onChange={handleSearch} value={searchValue} /> 
+    <InputGroup.Text id="basic-addon2"><CloseButton className="closeButton" onClick={() => setSearchValue("")}/> </InputGroup.Text>
+  </InputGroup>
+      </Col>
+
+
+
+        <Col xl={4} className="mt-1" >
+          <Form className="d-flex justify-content-evenly">
+            <div className="form-check  text-start" >
               <input className="form-check-input" type="radio" name="flexRadioDefault" id="flexRadioDefault1" onClick={() => setSortPrice("HigherLower")} defaultChecked={SortPrice === "HigherLower" ? true : false} />
               <label className="form-check-label" for="flexRadioDefault1" style={{ width: "120px" }}>
                 Lower to Higher
               </label>
             </div>
-            <div className="form-check  text-start" style={{ paddingTop: "10px" }}>
+            <div className="form-check  text-start">
               <input className="form-check-input" type="radio" name="flexRadioDefault" id="flexRadioDefault2" onClick={() => setSortPrice("LowerHigher")} defaultChecked={SortPrice === "LowerHigher" ? true : false} />
               <label className="form-check-label" style={{ width: "120px" }} for="flexRadioDefault2">
                 Higher to Lower
               </label> <br />
             </div>
-            <Button className="btn btn-sm btn-danger mt-2" type="reset" onClick={() => setSortPrice(false)}>
+            <Button className="btn btn-sm btn-danger py-0"  type="reset" onClick={() => setSortPrice(false)}>
               Unsorted
             </Button>
           </Form>
--- src/contexts/ProductContext.js
+++ src/contexts/ProductContext.js
@@ -11,6 +11,7 @@ const ProductProvider = ({ children }) => {
     const [ProductCategory, setCategory] = useState({ Motherboard: true, Video_Card: true, Processor: true })
     const [SortPrice, setSortPrice] = useState(false)
     const [isLoaded, setIsLoaded] = useState(false)
+    const [searchValue, setSearchValue] = useState("");
 
 
     useEffect(() => {
@@ -20,24 +21,26 @@ const ProductProvider = ({ children }) => {
 
     const filteredProducts = useMemo(() => {
         if (SortPrice === "HigherLower") {
-            return state.filter((product) => ProductCategory[product.category] === true).sort((a, b) => (a.price - b.price > 0 ? 1 : -1))
+            return state.filter((product) => ProductCategory[product.category] === true).sort((a, b) => (a.price - b.price > 0 ? 1 : -1)).filter((product) => product.name.toLowerCase().includes(searchValue.toLowerCase().trim()))
         }
         else if (SortPrice === "LowerHigher") {
-            return state.filter((product) => ProductCategory[product.category] === true).sort((a, b) => (a.price - b.price < 0 ? 1 : -1))
+            return state.filter((product) => ProductCategory[product.category] === true).sort((a, b) => (a.price - b.price < 0 ? 1 : -1)).filter((product) => product.name.toLowerCase().includes(searchValue.toLowerCase().trim()))
         }
         else {
-            return state.filter((product) => ProductCategory[product.category] === true)
+            return state.filter((product) => ProductCategory[product.category] === true).filter((product) => product.name.toLowerCase().includes(searchValue.toLowerCase().trim()))
         }
 
-    }, [state, ProductCategory, SortPrice])
+    }, [state, ProductCategory, SortPrice, searchValue])
 
 
     useEffect(() => {
         console.log(filteredProducts)
     }, [filteredProducts])
 
+  
 
-    return <ProductContext.Provider value={{ filteredProducts, isLoaded, ProductCategory, setCategory, state, setSortPrice, SortPrice }}>{children}</ProductContext.Provider>
+
+    return <ProductContext.Provider value={{ filteredProducts, isLoaded, ProductCategory, setCategory, state, setSortPrice, SortPrice, setSearchValue, searchValue }}>{children}</ProductContext.Provider>
 }
 
 const useProduct = () => useContext(ProductContext)
